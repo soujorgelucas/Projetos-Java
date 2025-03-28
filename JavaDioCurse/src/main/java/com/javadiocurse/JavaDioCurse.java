c831abb54653fccbe6cd6508464dbb7572ad0a9b
Jorge Lucas <jorgedesousa07@gmail.com>
2025-03-28 14:40:41 -0300

    primeiro commit

---
+++ JavaDioCurse/src/main/java/com/javadiocurse/JavaDioCurse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javadiocurse;
+
+/**
+ *
+ * @author jorge
+ */
+public class JavaDioCurse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
